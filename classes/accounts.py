93c6bb7687dfd4c7a00655192ca9c9e1f223ebac
GS31412032 <goldsmith3141@gmail.com>
2026-04-21 09:58:25 +0100

    Added 1st unit testing

---
+++ classes/accounts.py
@@ -0,0 +1,8 @@
+
+class Account():
+
+    def __init__(self, name, account_number, sort_code, balance):
+        self.name = name
+        self.account_number = account_number
+        self.sort_code = sort_code
+        self.balance = balance
--- test_suite.py
+++ test_suite.py
@@ -1,2 +1,19 @@
 # A test suite to ensure test driven development is followed
 import pytest
+from classes.accounts import Account
+
+@pytest.mark.parametrize("name, account_number, sort_code, balance", [
+    ('John Penny', 31412032, '40-47-84', 2500),
+    ('Jane Smith', 12345678, '20-30-40', 1000),
+])
+def test_account_class(name, account_number, sort_code, balance):
+    account = Account(name, account_number, sort_code, balance)
+    assert account.name == name
+    assert account.account_number == account_number
+    assert account.sort_code == sort_code
+    assert account.balance == balance
+
+def test_account_functions():
+    print('hello')
+
+    
