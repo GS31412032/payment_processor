df623dc727a2287064ae664cf12cc852ae23064a
GS31412032 <goldsmith3141@gmail.com>
2026-04-21 10:53:40 +0100

    Completion of Account Class

--- classes/accounts.py
+++ classes/accounts.py
@@ -6,3 +6,33 @@ class Account():
         self.account_number = account_number
         self.sort_code = sort_code
         self.balance = balance
+        self.transactions = []
+    
+    def deposit(self, deposit_amount):
+        self.balance += deposit_amount
+        transaction = {
+                'transaction_type': 'deposit',
+                'transaction_amount': deposit_amount
+            }
+        self.transactions.append(transaction)
+
+    def withdraw(self, withdraw_amount):
+        overdraft_limit = -1000
+        if self.balance - withdraw_amount > overdraft_limit:
+            self.balance -= withdraw_amount
+            transaction = {
+                'transaction_type': 'withdrawl',
+                'transaction_amount': withdraw_amount
+            }
+            self.transactions.append(transaction)
+        else:
+            print(f'Withdraw Amount would surpass Overdraft Limit: {overdraft_limit}')
+
+    def check_balance(self):
+        print(f'Your Current Balance: {self.balance}')
+    
+    def transaction_history(self):
+        for transaction in self.transactions:
+            print(transaction)
+        return self.transactions
+
--- test_suite.py
+++ test_suite.py
@@ -13,7 +13,34 @@ def test_account_class(name, account_number, sort_code, balance):
     assert account.sort_code == sort_code
     assert account.balance == balance
 
-def test_account_functions():
-    print('hello')
+@pytest.fixture
+def test_account():
+    return Account('John Penny', 31412032, '40-47-84', 2500)
+
+def test_account_deposit(test_account):
+    # Test Deposit
+    inital_balance = test_account.balance
+    test_account.deposit(1000)
+    assert test_account.balance > inital_balance
+
+def test_account_withdraw(test_account):
+    initial_balance = test_account.balance
+    test_account.withdraw(500)
+    assert test_account.balance == initial_balance - 500
+
+def test_account_overdraft(test_account, capsys):
+    test_account.withdraw(10000)
+    captured = capsys.readouterr()
+    assert 'Overdraft' in captured.out
+
+def test_transaction_log(test_account):
+    test_account.deposit(500)
+    transactions = test_account.transactions
+    assert len(transactions) > 0
+    assert transactions[0]['transaction_type'] == 'deposit'
+    assert transactions[0]['transaction_amount'] == 500
+
+    
+
 
     
